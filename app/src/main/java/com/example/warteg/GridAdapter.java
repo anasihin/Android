96c9c4b386d1ea1629dfa3e3d3221fd46d425c45
anasihin <asep.nasihin@yahoo.com>
2018-05-28 22:56:40 +0700

    update gradle

---
+++ app/src/main/java/com/example/warteg/GridAdapter.java
@@ -0,0 +1,26 @@
+package com.example.warteg;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
+
+
+    @Override
+    public GridAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(GridAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.Adapter{
+    }
+}
